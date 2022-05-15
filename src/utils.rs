ee75c91d284f6e088b86233d2e9fcca0b688e5c1
Kris <kristian_dilov@abv.bg>
2022-05-15 13:35:46 +0100

    Fix functions and tests

--- src/utils.rs
+++ src/utils.rs
@@ -1,20 +1,23 @@
 
-// def as_int(f: float) -> int:
-//      return int(round(f))
-// from time import time
+use std::time::SystemTime;
 
 fn as_int(f: f32) -> u32 {
-        return f.parse::<u32>()
+        let int = f as u32;
+        return int
 }
 
-fn timestamp_ms() -> u32 {
-    return as_int()
+fn timestamp_ms() -> u64 {
+    let sys_time = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap();
+
+    return sys_time.as_secs() * 1000
 }
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+   
     #[test]
-    fn exploration() {
+    fn test_as_int() {
         assert_eq!(as_int(1.0), 1);
     }
 
